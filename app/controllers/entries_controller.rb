a000f9a831084ef391651296f5efc326144b0258
Maldon Meehan and Noah Yasskin <maldon@>
2016-09-21 10:19:17 -0700

    add New and Create Views for Entry

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -6,5 +6,23 @@ class EntriesController < ApplicationController
   def show
     @entry = Entry.find(params[:id])
   end
-  
+
+  def new
+    @entry = Entry.new
+  end
+
+  def create
+    @entry = Entry.new(entry_params)
+    if @entry.save
+      redirect_to entries_path
+    else
+      render :new
+    end
+  end
+
+private
+  def entry_params
+    params.require(:entry).permit(:day)
+  end
+
 end
