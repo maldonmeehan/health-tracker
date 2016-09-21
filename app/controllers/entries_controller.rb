aa5713b461f2251251a6b526b25be030c33866c6
Maldon Meehan and Noah Yasskin <maldon+noah@>
2016-09-21 10:44:12 -0700

    add update and delete for Entry

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -14,10 +14,32 @@ class EntriesController < ApplicationController
   def create
     @entry = Entry.new(entry_params)
     if @entry.save
+      flash[:notice] = "Entry successfully added!"
       redirect_to entries_path
     else
       render :new
     end
+  end
+
+  def edit
+    @entry = Entry.find(params[:id])
+  end
+
+  def update
+    @entry = Entry.find(params[:id])
+    if @entry.update(entry_params)
+      flash[:notice] = "Entry successfully edited!"
+      redirect_to entries_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @entry = Entry.find(params[:id])
+    @entry.destroy
+    flash[:notice] = "Entry successfully deleted!"
+    redirect_to entries_path
   end
 
 private
