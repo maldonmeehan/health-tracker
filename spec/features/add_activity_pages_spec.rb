d5dbadfb0f1391a05b585fcc1af0032480e97cab
Maldon Meehan and Noah Yasskin <maldon+noah@>
2016-09-21 15:23:34 -0700

    add passing intergration spec for gives an error when no entry is entered and step is left blank for activity

--- spec/features/add_activity_pages_spec.rb
+++ spec/features/add_activity_pages_spec.rb
@@ -12,4 +12,17 @@ describe "the add an activity process" do
     click_button "Create Activity"
     expect(page).to have_content "Your activity has been added!"
   end
+
+  it "gives an error when step is left blank for an activity." do
+    entry = FactoryGirl.create(:entry)
+    visit entries_path
+    click_link "2016-09-21"
+    click_link 'Add an Activity'
+    fill_in "Step", :with => ""
+    fill_in "Calorie", :with => "1234"
+    fill_in "Minute", :with => "123"
+    click_button "Create Activity"
+    expect(page).to have_content "errors"
+  end
+
 end
--- spec/features/add_entry_pages_spec.rb
+++ spec/features/add_entry_pages_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
 describe "the add an entry process" do
-  it "adds an activity" do
+  it "adds an entry" do
     visit entries_path
     click_link "New Entry"
     fill_in "Day", with: '2016/09/21'
     click_button "Create Entry"
     expect(page).to have_content "Entry successfully added!"
+  end
+
+  it "it gives an error when day is entered for entry" do
+    visit entries_path
+    click_link "New Entry"
+    fill_in "Day", with: ''
+    click_button "Create Entry"
+    expect(page).to have_content "errors"
   end
 end
