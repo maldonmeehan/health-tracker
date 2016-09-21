e867e1dc262fc9564be606e64585fef20303e418
Maldon Meehan and Noah Yasskin <maldon+noah@>
2016-09-21 11:35:07 -0700

    add delete and update for activity

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -8,9 +8,33 @@ class ActivitiesController < ApplicationController
     @entry = Entry.find(params[:entry_id])
     @activity = @entry.activities.new(activity_params)
     if @activity.save
+      flash[:notice] = "Your activity has been added!"
       redirect_to entry_path(@activity.entry)
     else
       render :new
+    end
+  end
+
+  def edit
+    @entry = Entry.find(params[:entry_id])
+    @activity = Activity.find(params[:id])
+  end
+
+  def destroy
+    @activity = Activity.find(params[:id])
+    @activity.destroy
+    flash[:notice] = "Your activity has been deleted!"    
+    redirect_to entry_path(@activity.entry)
+  end
+
+  def update
+    @entry = Entry.find(params[:entry_id])
+    @activity = Activity.find(params[:id])
+    if @activity.update(activity_params)
+      flash[:notice] = "Your activity has been edited!"
+      redirect_to entry_path(@activity.entry)
+    else
+      render :edit
     end
   end
 
